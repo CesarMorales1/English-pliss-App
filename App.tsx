e1617a4c813d58cfadc362966fc6f27ba4686f35
miniproyecto <yurimar2003@gmail.com>
2024-05-11 18:42:35 -0400

    vista yuri clases

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { HomeScreen } from './src/Presentation/views/home/home';
 import RegisterScreen from './src/Presentation/views/register/Register';
+import VideoClassScreen from './src/Presentation/views/videoClass/VideoClass';
 
 export type RootStackParamList = {
   //aqui definimos que tipos de datos van a recibir esas pantallas
   HomeScreen: undefined,
   RegisterScreen: undefined,
+  VideoClassScreen: undefined,
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -18,6 +20,15 @@ const App = () => {
       <Stack.Navigator screenOptions={{
         headerShown: false
       }}>
+        {/* LLAMADO AL VIDEO */}
+        {<Stack.Screen
+          name="VideoClassScreen"
+          component={VideoClassScreen}
+          options={{
+            headerShown: true,
+            title: 'VideoClassScreen'
+          }}
+        />}
         {/* LLAMADO AL LOGIN */}
         <Stack.Screen
           name="HomeScreen"
--- src/Presentation/theme/AppTheme.ts
+++ src/Presentation/theme/AppTheme.ts
@@ -2,9 +2,10 @@ import { StyleSheet } from "react-native";
 
 export const MyColors = {
 
-    primary: '#1d2147', //azul
+    primary: '#1d2147', //azul oscuuro
     secondary: '#FFFFFF', //blanco
     tertiary: '#000000', //negro
-    background: '#EBEBEB', //gris, para inputs
+    background: '#EBEBEB', //gris, para inputs o backgrounds
+    grey: '#9393A3', //gris, para inputs o backgrounds
 
 }
