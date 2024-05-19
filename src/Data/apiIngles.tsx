82b61e074d67ff066d46734e55d2d085ada56306
EmmanuelChacon-ve <emmanuelechra@gmail.com>
2024-05-18 23:07:00 -0400

    Roles has user

--- src/Data/apiIngles.tsx
+++ src/Data/apiIngles.tsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const ApiIngles = axios.create(
-    {
-        baseURL: 'http://192.168.0.108:3000/v1',
-        headers: 
-        {
-            "Content-Type": 'application/json'
-        }
-    })
+const ApiIngles = axios.create({
+  baseURL: "http://192.168.1.104:3000/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export {ApiIngles};
+export { ApiIngles };
--- src/Presentation/views/register/Register.tsx
+++ src/Presentation/views/register/Register.tsx
@@ -1,125 +1,161 @@
-import React, { useEffect } from 'react'
-import { Image, ScrollView, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native'
-import { RoundedButton } from '../../../Presentation/components/RoundedButton';
-import useViewModel from './ViewModel'
-import { CustomTextInput } from '../../components/CustomTextInput';
+import React, { useEffect } from "react";
+import {
+  Image,
+  ScrollView,
+  Text,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { RoundedButton } from "../../../Presentation/components/RoundedButton";
+import useViewModel from "./ViewModel";
+import { CustomTextInput } from "../../components/CustomTextInput";
 import styles from "./Styles";
-
+import { Picker } from "@react-native-picker/picker";
 
 export default function RegisterScreen() {
+  const {
+    roles,
+    full_name,
+    email,
+    numero,
+    password,
+    confirmPassword,
+    onChange,
+    register,
+    errorMessage,
+    pickImage,
+    image,
+  } = useViewModel();
 
-  const {full_name, email, numero, password, confirmPassword, onChange, register, errorMessage, pickImage,image}= useViewModel();
+  useEffect(() => {
+    if (errorMessage) {
+      ToastAndroid.show(errorMessage, ToastAndroid.LONG);
+    }
+  }, [errorMessage]);
 
-  useEffect(() => 
-    {
-      if(errorMessage)
-        {
-          ToastAndroid.show(errorMessage,ToastAndroid.LONG);
-        }
-    },[errorMessage])
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../../../assets/background-login.png")}
+        style={styles.imageBackground}
+      />
 
-  return (  
-      <View style={styles.container}>
-        <Image
-          source={require('../../../../assets/background-login.png')}
-          style={styles.imageBackground}
-        />
-    
-        {/* LOGO SUPERIOR CENTRAL */}
-        <View style={styles.logoContainer}>
-          <TouchableOpacity onPress={() => pickImage()}>
-            {
-              image === ""?
-              <Image
+      {/* LOGO SUPERIOR CENTRAL */}
+      <View style={styles.logoContainer}>
+        <TouchableOpacity onPress={() => pickImage()}>
+          {image === "" ? (
+            <Image
               source={require("../../../../assets/user_image.png")}
               style={styles.logoImage}
             />
-            :
+          ) : (
+            <Image source={{ uri: image }} style={styles.logoImage} />
+          )}
+        </TouchableOpacity>
+        <Text style={styles.logoText}>Select a picture</Text>
+      </View>
+
+      {/* COMIENZA FORMULARIO */}
+      <View style={styles.form}>
+        <ScrollView>
+          <Text style={styles.formText}>Sign Up!</Text>
+
+          {/* Select Rol */}
+          <View style={styles.formSelect}>
             <Image
-            source={{uri: image}}
-            style={styles.logoImage}
-          />
-            }
+              style={styles.formIcon}
+              source={require("../../../../assets/user.png")}
+            />
+            <Picker
+              style={styles.formPicker}
+              selectedValue={null} // You can manage this state if needed
+              onValueChange={(itemValue) => onChange("id_rol", itemValue)}
+            >
+              <Picker.Item
+                label="Selecciona un Rol"
+                value={null}
+                enabled={false}
+                style={styles.titlePickerItem}
+              />
+              {roles.map((role) => (
+                <Picker.Item
+                  key={role.id_rol}
+                  label={role.name_rol}
+                  value={role.id_rol}
+                />
+              ))}
+            </Picker>
+          </View>
 
-          </TouchableOpacity>
-          <Text style={styles.logoText}>Select a picture</Text>
-        </View>
+          {/* COMIENZA SEGUNDO INPUT */}
+          <Text style={styles.formTextTitleInput}>Full name</Text>
 
-        {/* COMIENZA FORMULARIO */}
-          <View style={styles.form}>    
-            <ScrollView>
-            <Text style={styles.formText}>Sign Up!</Text>
-
-            {/* COMIENZA SGUNDO INPUT */}
-            <Text style={styles.formTextTitleInput}>Full name</Text>
-
-            <CustomTextInput
-            image={require('../../../../assets/my_user.png')}
-            placeholder='Full name'
-            keyboardType='default'
+          <CustomTextInput
+            image={require("../../../../assets/my_user.png")}
+            placeholder="Full name"
+            keyboardType="default"
             value={full_name}
-            property='full_name'
+            property="full_name"
             onChangeText={onChange}
-            />
+          />
 
-            {/* COMIENZA TERCER INPUT */}
-            <Text style={styles.formTextTitleInput}>Email</Text>
+          {/* COMIENZA TERCER INPUT */}
+          <Text style={styles.formTextTitleInput}>Email</Text>
 
-            <CustomTextInput
-            image={require('../../../../assets/email.png')}
-            placeholder='example@gmail.com'
-            keyboardType='email-address'
+          <CustomTextInput
+            image={require("../../../../assets/email.png")}
+            placeholder="example@gmail.com"
+            keyboardType="email-address"
             value={email}
-            property='email'
+            property="email"
             onChangeText={onChange}
-            />
+          />
 
-            {/* COMIENZA CUARTO INPUT */}
-            <Text style={styles.formTextTitleInput}>Your phone number</Text>
+          {/* COMIENZA CUARTO INPUT */}
+          <Text style={styles.formTextTitleInput}>Your phone number</Text>
 
-            <CustomTextInput
-            image={require('../../../../assets/phone.png')}
-            placeholder='(+12) 345-67890'
-            keyboardType='numeric'
+          <CustomTextInput
+            image={require("../../../../assets/phone.png")}
+            placeholder="(+12) 345-67890"
+            keyboardType="numeric"
             value={numero}
-            property='numero'
+            property="numero"
             onChangeText={onChange}
-            />
+          />
 
-            {/* COMIENZA QUINTO INPUT */}
-            <Text style={styles.formTextTitleInput}>Password</Text>
+          {/* COMIENZA QUINTO INPUT */}
+          <Text style={styles.formTextTitleInput}>Password</Text>
 
-            <CustomTextInput
-            image={require('../../../../assets/password.png')}
-            placeholder='More than 8 characters'
-            keyboardType='default'
+          <CustomTextInput
+            image={require("../../../../assets/password.png")}
+            placeholder="More than 8 characters"
+            keyboardType="default"
             secureTextEntry={true}
             value={password}
-            property='password'
+            property="password"
             onChangeText={onChange}
-            />
+          />
 
-            {/* COMIENZA SEXTO INPUT */}
-            <Text style={styles.formTextTitleInput}>Confirm password</Text>
+          {/* COMIENZA SEXTO INPUT */}
+          <Text style={styles.formTextTitleInput}>Confirm password</Text>
 
-            <CustomTextInput
-            image={require('../../../../assets/confirm_password.png')}
-            placeholder='Confirm password'
-            keyboardType='default'
+          <CustomTextInput
+            image={require("../../../../assets/confirm_password.png")}
+            placeholder="Confirm password"
+            keyboardType="default"
             secureTextEntry={true}
             value={confirmPassword}
-            property='confirmPassword'
+            property="confirmPassword"
             onChangeText={onChange}
-            />
+          />
 
-      
-            {/* COMIENZA BOTON */}
-            <View>
-              <RoundedButton text='Sign up' onPress={() => register()} />
-            </View>
-            </ScrollView>
+          {/* COMIENZA BOTON */}
+          <View>
+            <RoundedButton text="Sign up" onPress={() => register()} />
           </View>
-      </View> 
-  );        
-}      
-    
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
